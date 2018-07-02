e98164f6b2251971a98913bc10412489b417d145
tretiakoff <c.tretiakoff@gmail.com>
2018-07-02 14:53:25 +0200

    just learning...

---
+++ app/src/main/java/com/example/tretiakoff/sifg2/SecondActivity.java
@@ -0,0 +1,24 @@
+package com.example.tretiakoff.sifg2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    TextView welcomeText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        welcomeText = findViewById(R.id.welcomeText);
+        //welcomeText.setText("Hello Gaoussou");
+
+        welcomeText.postDelayed(new Runnable() {
+            public void run() {
+                welcomeText.setText("Hello Gaoussou.");
+            }
+        }, 5000);
+    }
+}
