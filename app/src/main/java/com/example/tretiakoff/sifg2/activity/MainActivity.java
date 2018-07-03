c9667c270a97dd4552eb307d2b0361c318d4eca6
tretiakoff <c.tretiakoff@gmail.com>
2018-07-03 14:16:19 +0200

    wip

--- app/src/main/java/com/example/tretiakoff/sifg2/activity/MainActivity.java
+++ app/src/main/java/com/example/tretiakoff/sifg2/activity/MainActivity.java
@@ -1,15 +1,29 @@
 package com.example.tretiakoff.sifg2.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.tretiakoff.sifg2.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button chatViewBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        chatViewBtn = findViewById(R.id.chatBtn);
+
+        chatViewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent chatActivityIntent = new Intent(MainActivity.this, MessageListActivity.class);
+                startActivity(chatActivityIntent);
+            }
+        });
     }
 }
