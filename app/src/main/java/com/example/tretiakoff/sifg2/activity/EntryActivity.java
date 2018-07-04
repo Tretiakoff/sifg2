7a944cd5f8a2e33f6c4ce2ced05ac69bafa86127
Gaoussou <gaoussou.ony@gmail.com>
2018-07-04 20:56:14 +0200

    second screen wip

---
+++ app/src/main/java/com/example/tretiakoff/sifg2/activity/EntryActivity.java
@@ -0,0 +1,15 @@
+package com.example.tretiakoff.sifg2.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.tretiakoff.sifg2.R;
+
+public class EntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry);
+    }
+}
