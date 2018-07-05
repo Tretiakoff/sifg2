dc21526b88c074e150ed5a2cf045042de9e349b3
Gaoussou <gaoussou.ony@gmail.com>
2018-07-06 00:43:26 +0200

    single doctor

---
+++ app/src/main/java/com/example/tretiakoff/sifg2/activity/single_doctorActivity.java
@@ -0,0 +1,13 @@
+package com.example.tretiakoff.sifg2.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.tretiakoff.sifg2.R;
+public class single_doctorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_doctor);
+    }
+}
