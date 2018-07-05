d9fa9e37676575ed4ce205b627da6561c22060a2
Quentin Massias <quentin.massias28630@gmail.com>
2018-07-05 14:14:26 +0200

    add button to go to next screen

--- app/src/main/java/com/example/tretiakoff/sifg2/activity/MainActivity.java
+++ app/src/main/java/com/example/tretiakoff/sifg2/activity/MainActivity.java
@@ -1,15 +1,30 @@
 package com.example.tretiakoff.sifg2.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.tretiakoff.sifg2.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button menuViewBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        menuViewBtn = findViewById(R.id.nextPage);
+
+        menuViewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent chatActivityIntent = new Intent(MainActivity.this, EntryActivity.class);
+                startActivity(chatActivityIntent);
+            }
+        });
     }
 }
