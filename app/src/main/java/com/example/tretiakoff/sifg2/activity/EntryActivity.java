4921fd4b73fd8bb7d61d33666b4b5a41172ff089
tretiakoff <c.tretiakoff@gmail.com>
2018-07-06 02:27:44 +0200

    toolbar

--- app/src/main/java/com/example/tretiakoff/sifg2/activity/EntryActivity.java
+++ app/src/main/java/com/example/tretiakoff/sifg2/activity/EntryActivity.java
@@ -1,10 +1,19 @@
 package com.example.tretiakoff.sifg2.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.tretiakoff.sifg2.R;
 
@@ -17,6 +26,25 @@ public class EntryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry);
 
+
+//        ActionBar.LayoutParams lp = new ActionBar.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT, Gravity.LEFT | Gravity.CENTER_VERTICAL);
+//        View customNav = LayoutInflater.from(this).inflate(R.layout.action_bar, null); // layout which contains your button.
+//        bar.setCustomView(customNav, lp);
+//        bar.setDisplayHomeAsUpEnabled(true);
+
+
+//        ImageButton imageButton = (ImageButton) mCustomView
+//                .findViewById(R.id.imageButton);
+//        imageButton.setOnClickListener(new OnClickListener() {
+//
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(getApplicationContext(), "Refresh Clicked!",
+//                        Toast.LENGTH_LONG).show();
+//            }
+//        });
+
+//        getSupportActionBar().setCustomView(R.layout.action_bar);
         pathologyActivityBtn = findViewById(R.id.problem_Btn);
         pathologyActivityBtn.setOnClickListener(new View.OnClickListener() {
             @Override
